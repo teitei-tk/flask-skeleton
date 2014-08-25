248a5a143b05caab075cbdda51bf341cf0ebd30d
teitei-tk <teitei.tk@gmail.com>
2014-08-25 22:55:04 +0900

    add : find method

--- lib/model.py
+++ lib/model.py
@@ -1,7 +1,22 @@
 # coding: utf-8
-from peewee import ( Model, )
+from peewee import ( Model,  )
 from application import bootstrap
 
-class BaseModel(Model):
+class BaseModelMixin(object):
+    @property
+    def db(self):
+        return bootstrap.db
+
+    @classmethod
+    def find(cls, key, column=None):
+        try:
+            if not column:
+                column = cls.id
+            return cls.get(column == key)
+
+        except cls.DoesNotExist:
+            return None
+
+class BaseModel(Model, BaseModelMixin):
     class Meta:
         database = bootstrap.db
