911e8fde2a0351c3f3340aebf9c11bdb89a51e8d
teitei-tk <teitei.tk@gmail.com>
2014-08-26 22:10:51 +0900

    rewrite find method

--- lib/model.py
+++ lib/model.py
@@ -8,14 +8,24 @@ class BaseModelMixin(object):
         return bootstrap.db
 
     @classmethod
-    def find(cls, key, column=None):
+    def find(cls, key, column=None, offset=1, limit=10):
         try:
             if not column:
                 column = cls.id
-            return cls.get(column == key)
+
+            result = [x for x in ( cls.select().where(column == key).
+                        order_by(column == key).paginate(offset, limit).
+                        aggregate_rows() ) ]
+            next = None
+            if result.__len__() == limit:
+                next = offset + limit
+            return result, next
 
         except cls.DoesNotExist:
-            return None
+            return [], None
+
+        except ValueError:
+            return [], None
 
 class BaseModel(Model, BaseModelMixin):
     class Meta:
