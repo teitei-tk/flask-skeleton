0fabe36900696539b0817b33d4220da099bfe3b7
teitei-tk <teitei.tk@gmail.com>
2014-09-03 17:39:36 +0900

    test get_by_column and find methods

--- tests/model.py
+++ tests/model.py
@@ -6,7 +6,6 @@ from peewee import ( CharField, )
 from application import ( bootstrap, )
 from lib.model import ( BaseModel, )
 
-
 class TestDatabaseModel(BaseModel):
     name = CharField()
 
@@ -37,5 +36,29 @@ class TestModel(unittest.TestCase):
         cursor.close()
         self.connection.close()
 
-    def test_insert(self):
-        assert True
+    def test_get_by_column(self):
+        # fail case
+        result = TestDatabaseModel.get_by_column("test", TestDatabaseModel.name) 
+        self.assertEqual(result, None)
+
+        # success case
+        TestDatabaseModel.create(name = "test")
+        result = TestDatabaseModel.get_by_column("test", TestDatabaseModel.name)
+        self.assertNotEqual(result, None)
+
+    def test_find(self):
+        # fail case
+        result, next = TestDatabaseModel.find('test', TestDatabaseModel.name)
+        self.assertFalse(result)
+        self.assertFalse(next)
+
+        # success case
+        TestDatabaseModel.create(name = "test")
+        result, next = TestDatabaseModel.find('test', TestDatabaseModel.name)
+        self.assertNotEqual(result, [])
+        self.assertEqual(next, None)
+
+        # get next skip
+        result, next = TestDatabaseModel.find('test', TestDatabaseModel.name, limit=1)
+        self.assertNotEqual(result, [])
+        self.assertNotEqual(next, None)
