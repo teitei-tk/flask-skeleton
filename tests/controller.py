cdaef5d5bde7d29381270a7d21ce6c10d4bb8255
teitei-tk <teitei.tk@gmail.com>
2014-09-03 22:55:17 +0900

    controller test passing

--- tests/controller.py
+++ tests/controller.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import jinja2
 import simplejson as json
 from flask import ( g, Blueprint, make_response, request, )
 
@@ -11,14 +12,23 @@ class TestJsonRender(TestBaseController):
     def preforward(self):
         return self.render_json({"value" : 1})
 
-@controller_module.route("/test")
+class TestTemplateRender(TestBaseController):
+    def preforward(self):
+        return self.render_template("test.html")
+
+@controller_module.route("/test_json_render")
 def json_render():
     return TestJsonRender.action()
+
+@controller_module.route("/test_template_render")
+def template_render():
+    return TestTemplateRender.action()
 
 class TestController(TestBase):
     def initialize(self):
         super(TestController, self).initialize()
         bootstrap.set_routing(controller_module)
+        bootstrap.flask.jinja_loader = jinja2.FileSystemLoader("tests/template/")
 
     def get_response(self, app):
         rv = app.preprocess_request()
@@ -31,8 +41,16 @@ class TestController(TestBase):
         return response
 
     def test_json_render(self):
-        with app.test_request_context("/test"):
+        with app.test_request_context("/test_json_render"):
             response = self.get_response(app)
 
             self.assertEqual(response.mimetype, "text/json")
             self.assertEqual(response.data.decode('utf-8'), json.dumps({"value" : 1}))
+
+
+    def test_template_render(self):
+        with app.test_request_context("/test_template_render"):
+            response = self.get_response(app)
+
+            self.assertEqual(response.mimetype, "text/html")
+            self.assertEqual(response.data.decode('utf-8'), "")
