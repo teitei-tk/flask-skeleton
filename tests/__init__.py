6f859790b8f9fd31acb010914e7fc800f136f0a1
teitei-tk <teitei.tk@gmail.com>
2014-09-03 19:24:17 +0900

    add : test base class

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,31 @@
 # coding: utf-8
+
+import unittest
+from pymysql import ( connect, )
+
+from application import ( bootstrap, )
+
+class TestBase(unittest.TestCase):
+    test_db_name = "test_db"
+
+    def setUp(self):
+        bootstrap.config['DATABASE_SETTING']['db_name'] = self.test_db_name
+        db_setting = bootstrap.config['DATABASE_SETTING']
+
+        self.connection = connect(host=db_setting['host'], port=db_setting['port'], 
+                user=db_setting['user'], passwd=db_setting['password'])
+
+        self.initialize()
+
+    def initialize(self):
+        cursor = self.connection.cursor()
+        cursor.execute("CREATE DATABASE {0}".format(self.test_db_name))
+        cursor.close()
+
+    def tearDown(self):
+        cursor = self.connection.cursor()
+        cursor.execute("DROP DATABASE {0}".format(self.test_db_name))
+        cursor.close()
+        self.connection.close()
+
+
--- tests/model.py
+++ tests/model.py
@@ -3,26 +3,16 @@ import unittest
 from pymysql import ( connect, )
 from peewee import ( CharField, )
 
+from tests import ( TestBase, )
 from application import ( bootstrap, )
 from lib.model import ( BaseModel, )
 
 class TestDatabaseModel(BaseModel):
     name = CharField()
 
-class TestModel(unittest.TestCase):
-    def setUp(self):
-        bootstrap.config['DATABASE_SETTING']['db_name'] = 'test_db'
-        db_setting = bootstrap.config['DATABASE_SETTING']
-
-        self.connection = connect(host=db_setting['host'], port=db_setting['port'], 
-                user=db_setting['user'], passwd=db_setting['password'])
-
-        self.initialize()
-
+class TestModel(TestBase):
     def initialize(self):
-        cursor = self.connection.cursor()
-        cursor.execute("CREATE DATABASE test_db")
-        cursor.close()
+        super(TestModel, self).initialize()
 
         bootstrap.db.connect()
         bootstrap.db.create_tables([TestDatabaseModel])
@@ -31,10 +21,7 @@ class TestModel(unittest.TestCase):
         bootstrap.db.drop_tables([TestDatabaseModel])
         bootstrap.db.close()
 
-        cursor = self.connection.cursor()
-        cursor.execute("DROP DATABASE test_db")
-        cursor.close()
-        self.connection.close()
+        super(TestModel, self).tearDown()
 
     def test_get_by_column(self):
         # fail case
