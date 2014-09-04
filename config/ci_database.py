b37993944d85706de7ef303043013f2c170b1f0b
teitei-tk <teitei.tk@gmail.com>
2014-09-05 01:24:33 +0900

    gomi

--- config/ci_database.py
+++ config/ci_database.py
@@ -2,7 +2,7 @@ import os
 
 CI_DATABASE_SETTING = dict(
     db_name     = os.environ.get('WERCKER_MYSQL_DATABASE'),
-    host        = os.environ.get('WERCKER_MYSQL_HOST') or 0,
+    host        = os.environ.get('WERCKER_MYSQL_HOST'),
     port        = int(os.environ.get('WERCKER_MYSQL_PORT') or 0),
     user        = os.environ.get('WERCKER_MYSQL_USERNAME'),
     password    = os.environ.get('WERCKER_MYSQL_PASSWORD'),
