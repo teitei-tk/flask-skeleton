6ad50d663a500c8aa2323e512f5b014ca2ace86b
teitei-tk <teitei.tk@gmail.com>
2014-09-18 05:37:54 +0900

    add : log class

--- lib/logger.py
+++ lib/logger.py
@@ -1,7 +1,19 @@
 # coding: utf-8
+import datetime
 
 class Log(object):
-    pass
+    def __init__(self, message, code=1):
+        self.message = message
+        self.code    = code
+
+    def to_string(self, now=None):
+        if not now:
+            now = datetime.datetime.now()
+        return "{0} [{1}]:{2}".format(
+                now.strftime("%Y-%m-%d %H:%m:%S"), self.message, self.code)
+
+    def __str__(self):
+        return self.to_string()
 
 class Logger(object):
     pass
