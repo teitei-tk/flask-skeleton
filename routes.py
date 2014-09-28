c6e136890fd4e85f1e271e46bb25005e04c2da96
teitei-tk <teitei.tk@gmail.com>
2014-09-29 00:20:45 +0900

    comment

--- routes.py
+++ routes.py
@@ -1,13 +1,25 @@
 # coding: utf-8
+
 from collections import ( namedtuple, )
 
-Router = namedtuple('Router', ['url', 'import_path', 'endpoint'])
+"""
+Please add a tuple format the class you want to url management.
 
+example: 
+    stirng url
+    string routing class path
+    string endpoint name
+
+    ("/", "application.c.hoge.Foo", "foo")
+"""
 VIEW_DATAS = [
     ("/", "application.c.root.index.Index", "index"),
     ]
 
+# router class
+Router = namedtuple('Router', ['url', 'import_path', 'endpoint'])
 
+# managements modules
 ROUTING_MODULES = [
     Router(url=data[0], import_path=data[1], endpoint=data[2]) for data in VIEW_DATAS
     ]
